602611d3407f26cc8273e0fe14ec28721bccd9ec
Leigh Robertson <leighrobertson512@gmail.com>
2025-12-04 16:49:33 -0700

    adding additional files

---
+++ pipeline_observability/explorations/pipeline_registry_ddl.sql
@@ -0,0 +1,9 @@
+CREATE TABLE leigh_robertson_demo.observability.pipeline_registry (
+  pipeline_id STRING,
+  pipeline_name STRING,
+  domain STRING,
+  event_log_table STRING,
+  log_enabled BOOLEAN,
+  owner_email STRING,
+  created_at TIMESTAMP
+);
---
+++ pipeline_observability/explorations/silver_table_schema.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE TABLE leigh_robertson_demo.observability.silver.event_log_cleaned
+AS
+SELECT
+  current_timestamp() AS loaded_at,
+  '<pipeline_name>' AS pipeline_name,
+  event_type,
+  timestamp,
+  origin.update_id AS update_id,
+  origin.workspace_id,
+  origin.cloud,
+  details:flow_progress.metrics.backlog_bytes AS backlog_bytes,
+  details:flow_progress.data_quality.expectations AS dq_expectations,
+  details:user_action.action AS user_action,
+  details:autoscale.requested_num_executors AS autoscale_exec,
+  details:operation_progress.auto_loader_details.num_files_listed AS auto_loader_file_count
+FROM leigh_robertson_demo.observability.event_log_noaa_weather_ingestion_sample_pipeline
+CLUSTER BY event_type, pipeline_name;
