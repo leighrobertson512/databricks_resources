d13036da25f6fd5de68544371d92be55febf50bd
Leigh Robertson <leighrobertson512@gmail.com>
2025-12-05 12:57:43 -0700

    changing folder structure

--- pipeline_observability/transformations/pii_kpi_summary.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REFRESH STREAMING TABLE leigh_robertson_demo.observability.pii_kpi_summary AS
-SELECT
-  COUNT(DISTINCT table_name) AS total_tables_scanned,
-  COUNT(DISTINCT CASE WHEN is_pii = 'Yes' THEN table_name END) AS tables_with_pii,
-  COUNT(CASE WHEN is_pii = 'Yes' THEN 1 END) AS pii_columns,
-  COUNT(DISTINCT schema_name) AS total_schemas,
-  COUNT(*) FILTER (WHERE violation_type IS NOT NULL) AS total_violations
-FROM STREAM(leigh_robertson_demo.observability.pii_info)
-LEFT JOIN STREAM(leigh_robertson_demo.observability.pii_violations) USING (catalog_name, schema_name, table_name, column_name);
--- pipeline_observability/transformations/pii_violation_trend.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REFRESH STREAMING TABLE leigh_robertson_demo.observability.pii_violation_trend AS
-SELECT
-  current_date() AS snapshot_date,
-  violation_type,
-  COUNT(*) AS total
-FROM STREAM(leigh_robertson_demo.observability.pii_violations)
-GROUP BY violation_type;
